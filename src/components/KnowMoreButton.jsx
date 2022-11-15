b53679ccf84df6ecfb7fcf4e7d5771b46206b2a5
Chandrakant Pal <sanjay161097@gmail.com>
2022-11-15 22:55:17 +0530

    know more button

---
+++ src/components/KnowMoreButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const KnowMoreButton = () => {
+  return (
+    <button className="apply_btn py-3 px-8 md:px-11 text-white text-sm md:text-xl rounded-[20px] md:rounded-[25px] leading-7 md:leading-[50px] bg-black border border-pink">
+      Know More
+    </button>
+  );
+};
+
+export default KnowMoreButton;
